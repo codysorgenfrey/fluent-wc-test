87921f5c967effbdb5fef4717d165027668168e2
Cody Sorgenfrey <codysorgenfrey@gmail.com>
2024-06-06 09:59:02 -0700

    Working test to compare

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,24 @@
-import { setTheme } from '@fluentui/web-components';
+import { setThemeFor } from '@fluentui/web-components';
 import { webLightTheme } from '@fluentui/tokens';
+import { setTheme } from '@phoenixui/web-components';
+import { phoenixLightThemeWin11 } from '@phoenixui/themes';
 import '@fluentui/web-components/button.js';
-import '@fluentui/web-components/menu.js';
-import '@fluentui/web-components/menu-item.js';
-import '@fluentui/web-components/menu-button.js';
-
-setTheme(webLightTheme);
+import '@fluentui/web-components/divider.js';
+import '@phoenixui/web-components/button.js';
+import '@phoenixui/web-components/divider.js';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
-  <fluent-button>Click me</fluent-button>
-  <fluent-menu>
-    <fluent-menu-button slot="trigger">Menu</fluent-menu-button>
-    <fluent-menu-item>Item 1</fluent-menu-item>
-    <fluent-menu-item>Item 2</fluent-menu-item>
-  </fluent-menu>
+  <div id="fluent" style="display: flex; flex-direction: row; align-items: center; gap: 24px;">
+    <fluent-button>Click me</fluent-button>
+    <fluent-divider orientation="vertical"></fluent-divider>
+    <fluent-button appearance="primary">Click me</fluent-button>
+  </div>
+  <div id="phoenix" style="display: flex; flex-direction: row; align-items: center; gap: 24px;">
+    <phx-button>Click me</phx-button>
+    <phx-divider orientation="vertical"></phx-divider>
+    <phx-button appearance="primary">Click me</phx-button>
+  </div>
 `;
+
+setThemeFor(document.querySelector('#fluent')!, webLightTheme);
+setTheme(phoenixLightThemeWin11);
